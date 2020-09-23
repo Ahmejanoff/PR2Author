56d4dbb2bbfcbe52301e2d6e6571e2465cfcf47d
Ahmejanoff <e.ahmejanoff@gmail.com>
2020-09-23 19:01:04 +0300

    Initial commit

---
+++ src/ru/mirea/ikbo0819/pr2/TestAuthor.java
@@ -0,0 +1,10 @@
+package ru.mirea.ikbo0819.pr2;
+
+class Main
+{
+    public static void main(String[] args)
+    {
+        Author author1 = new Author("Jules Verne ", "JulesVerne@yandex.ru", 'M');
+        System.out.println(author1);
+    }
+}
